1863600f01142448be1130f1d369b6e46d830946
Jennifer Bishop <jennybishop2001@yahoo.co.uk>
2021-05-10 21:41:02 +0100

    writing out results to file

--- src/cord19_clean.py
+++ src/cord19_clean.py
@@ -73,6 +73,7 @@ if __name__ == '__main__':
         print('Cleaning & saving off only pubmed files to {}...'.format(post_path))
         with tqdm(total=df_len) as pbar:
             for i, row in df.iterrows():
+                print(i)
                 if i >= df_len:
                     break
                 pbar.update(1)
