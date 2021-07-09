85a0a74eb429e4c025990b6954655fba97724653
Jennifer Bishop <jennybishop2001@yahoo.co.uk>
2021-07-09 11:53:06 +0100

    testing test

--- src/models/data_loader.py
+++ src/models/data_loader.py
@@ -217,6 +217,7 @@ class DataIterator(object):
     def batch_buffer(self, data, batch_size):
         minibatch, size_so_far = [], 0
         for ex in data:
+            print(len(ex['src']))
             if ((len(ex['src'])==0) or (len(ex['src'])<512)):
                 continue
             ex = self.preprocess(ex, self.is_test)
