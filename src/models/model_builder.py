9515ddf8743f5897d83d9ab1888787f62b040c5e
Jennifer Bishop <jennybishop2001@yahoo.co.uk>
2021-08-13 16:13:21 +0100

    extra print

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -269,6 +269,7 @@ class ExtSummarizer(nn.Module):
         self.to(device)
 
     def forward(self, src, segs, clss, mask_src, mask_cls):
+        print(f"ExtSum: {np.shape(src)}")
         top_vec = self.RoBerta(src, segs, mask_src)
         sents_vec = top_vec[torch.arange(top_vec.size(0)).unsqueeze(1), clss]
         sents_vec = sents_vec * mask_cls[:, :, None].float()
