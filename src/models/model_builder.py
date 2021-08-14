87ced2f6b6fb17e8d72869963805a3276e885f7b
Jennifer Bishop <jennybishop2001@yahoo.co.uk>
2021-08-14 15:31:29 +0100

    adapters

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -182,15 +182,12 @@ class RoBerta(nn.Module):
                         self.model.load_adapter(args.adapter_path_pubmed_generative, load_as="mlm",with_head=False)
             if model == "longformer":
                 model_name = 'allenai/longformer-base-4096'
-                self.model = AutoModel.from_pretrained(model_name).to(device)
-                config = RobertaConfig.from_pretrained(
-                    model_name,
-                    num_labels=len(label_list),
-                )
+                # self.model = AutoModel.from_pretrained(model_name).to(device)
+                config = RobertaConfig.from_pretrained(model_name )
                 self.model = RobertaModelWithHeads.from_pretrained(
                     model_name,
                     config=config,
-                )
+                ).to(device)
                 if args.adapter_training_strategy != 'basic':
                     if args.adapter_training_strategy == 'both':
                         self.model.load_adapter(args.adapter_path_robert_generative, load_as="mlm",with_head=False)
